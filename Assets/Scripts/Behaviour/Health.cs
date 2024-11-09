e54294c639930478151f1d2806d3e50b24d9573b
Alexey <a_kelmakov@mail.ru>
2024-11-09 15:31:48 +0300

    Items

---
+++ Assets/Scripts/Behaviour/Health.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Health 
+{
+    private int _valueHealth;
+    private int _currentHealth;
+
+    public Health(int valueHealth)
+    {
+        ValueHealth = valueHealth;
+        CurrentHealth = valueHealth;
+    }
+
+    public int ValueHealth
+    {
+        get => _valueHealth;
+        set
+        {
+            if (value <= 0)
+            {
+                Debug.LogError($"¬ свойство ValueHealt, в классе {GetType().Name} задаетс€ значние меньше либо равно 0");
+            }
+            else _valueHealth = value;
+        }
+    }
+
+    public int CurrentHealth
+    {
+        get => _currentHealth;
+        set
+        {
+            if (value < 0)
+            {
+                _currentHealth = 0;
+                Debug.LogError("¬ свойство CurrentHealth передаетс€ значение меньше 0");
+            }
+            else
+            {
+                _currentHealth += value;
+
+                if(_currentHealth > _valueHealth)  
+                    _currentHealth = _valueHealth;
+            }
+        }
+    }
+}
--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -6,6 +6,9 @@ public class Ghost : MonoBehaviour
     private float DeadZone = 0.1f;
 
     [SerializeField] private float _speed;
+    [SerializeField] private int _healthValue;
+
+    private Health _health;
     private Player _player;
     private WalkPlayer _walkPlayer;
     private PullToMyselfItem _pullToMyselfItem;
@@ -15,6 +18,7 @@ public class Ghost : MonoBehaviour
         SetComponents();
         CheckForNullReferenceException();
 
+        _health = new Health(_healthValue);
         _player.Initialization(_walkPlayer, _pullToMyselfItem);
 
         SetValues();
@@ -24,6 +28,8 @@ public class Ghost : MonoBehaviour
     private void Update()
     {
         _player.Walk();
+
+        Debug.Log($"Çהמנמגüו: {_health.CurrentHealth}");
     }
 
     private void SetComponents()
@@ -36,13 +42,10 @@ public class Ghost : MonoBehaviour
     private void CheckForNullReferenceException()
     {
         if (_player == null)
-        {
             Debug.LogError("Player is null");
-        }
+
         if (_walkPlayer == null)
-        {
             Debug.LogError("WalkPlayer is null");
-        }
     }
 
     private void SetValues()
